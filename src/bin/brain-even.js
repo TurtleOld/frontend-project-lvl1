22c4a15b11a3cf0ae9c7933742455a31e00f334b
Shurpo Alexander <shurpo.alexander@gmail.com>
2020-01-07 22:19:15 +0300

    change brain-even insrc

--- src/bin/brain-even.js
+++ src/bin/brain-even.js
@@ -1,7 +1,24 @@
 #!/usr/bin/env node
 
-import { welcome } from './brain-games';
+import readlineSync from 'readline-sync';
 
-welcome;
+console.log('Welcome to the Brain Games!');
 
-export const comment = console.log('Answer "yes" if the number is even, otherwise answer "no".');
+console.log('Answer "yes" if the number is even, otherwise answer "no".');  
+
+console.log();
+
+const questions = readlineSync.question('May I have your name? ');
+
+console.log(`Hello, ${questions}`);
+console.log();
+const numberQuestion = readlineSync.question(`Question: ${Math.floor(Math.random() * 100)}`);
+const yourAnswer = readlineSync.question('Your answer: ');
+
+const parity = () => {
+  if (numberQuestion / 2) {
+    return 'yes';
+  }else {
+    return 'no';
+  }
+};
