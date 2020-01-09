c3ac05b6b331051f5e014c3c1ec27a663ecc2b6f
Shurpo Alexander <shurpo.alexander@gmail.com>
2020-01-09 21:01:48 +0300

    change file brain-even

--- src/bin/brain-even.js
+++ src/bin/brain-even.js
@@ -2,19 +2,35 @@
 
 import readlineSync from 'readline-sync';
 
-console.log('Welcome to the Brain Games!');
+const games = (rounds = 3) => {
+  console.log('Welcome to the Brain Games!');
 
-console.log('Answer "yes" if the number is even, otherwise answer "no".');  
+  console.log('Answer "yes" if the number is even, otherwise answer "no".');
 
-console.log();
+  console.log();
 
-const questions = readlineSync.question('May I have your name? ');
+  const userName = readlineSync.question('May I have your name? ');
 
-console.log(`Hello, ${questions}`);
-console.log();
-const numberQuestion = readlineSync.question(`Question: ${Math.floor(Math.random() * 100)}`);
-const yourAnswer = readlineSync.question('Your answer: ');
+  console.log(`Hello, ${userName}`);
+  console.log();
+  const numberQuestion = readlineSync.question(`Question: ${Math.floor(Math.random() * 100)}`);
+  const yourAnswer = readlineSync.question('Your answer: ');
 
-const parity = () => (numberQuestion / 2) ? 'yes' : 'no';
-
-console.log(parity())
+  const primeCheck = () => {
+    if (numberQuestion % 2) {
+      return 'yes';
+    }
+    return 'no';
+  };
+  let count = 1;
+  for (; count <= rounds; count += 1) {
+    if (yourAnswer === String(primeCheck)) {
+      console.log('Correct!');
+    } else {
+      console.log(`'${yourAnswer}' is wrong answer ;(. Correct answer was ${primeCheck()}.\nLet\`s try again, ${userName}!`);
+      return;
+    }
+  }
+  console.log(`Congratulation, ${userName}!`);
+};
+export default games();
