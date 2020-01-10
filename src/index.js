2c3b34d1221df4260c87e4417c15d4f161983e73
Shurpo Alexander <shurpo.alexander@gmail.com>
2020-01-10 20:34:27 +0300

    del code

--- src/index.js
+++ src/index.js
@@ -1,17 +1,1 @@
 #!/usr/bin/env node
-
-import readlineSync from 'readline-sync';
-
-
-const startGame = (description) => {
-  console.log("Welcome to the Brain Games!");
-  console.log(description);
-  console.log();
-
-  const userName = readlineSync.question("May I have your name? ");
-  console.log(`Hello, ${userName}`);
-  console.log();
-
-}
-
-export default startGame;
