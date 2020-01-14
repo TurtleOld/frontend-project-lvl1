ef60288229a5a2d313a8346e3bd0c8094c389169
Shurpo Alexander <shurpo.alexander@gmail.com>
2020-01-14 21:29:22 +0300

    Game GCD(NOD) DONE

--- src/bin/brain-gcd.js
+++ src/bin/brain-gcd.js
@@ -1,4 +1,4 @@
-#!/user/bin/env node
+#!/usr/bin/env node
 
 import gamesNod from '../games/brain-gcd';
 
--- src/games/brain-gcd.js
+++ src/games/brain-gcd.js
@@ -10,16 +10,16 @@ hello(userName);
 
 const gameNod = (rounds = 3) => {
   let count = 1;
-  for (; count < rounds; count += 1) {
+  for (; count <= rounds; count += 1) {
     const randomNum1 = Math.floor(Math.random() * 100);
     const randomNum2 = Math.floor(Math.random() * 100);
 
-    const nod = (n, m) => {
-      if(m > 0) { 
-        var k = n % m;
-        return nod(m, k); 
+    const nod = (a, b) => {
+      if(b > 0) { 
+        var c = a % b;
+        return nod(b, c); 
     } else { 
-      return Math.abs(n);  
+      return Math.abs(a);  
       }
     };
     const numTwo = `${randomNum1} ${randomNum2}`
