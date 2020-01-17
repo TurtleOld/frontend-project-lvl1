3def1f05f412257f131c40574f00ff561b0d6b16
Shurpo Alexander <shurpo.alexander@gmail.com>
2020-01-17 19:24:22 +0300

    change file

--- src/bin/brain-progression.js
+++ src/bin/brain-progression.js
@@ -1,5 +1,5 @@
 #!/usr/bin/env node
 
-import gamesProgress from '../games/brain-progression';
+import gameProgress from '../games/brain-progression';
 
-gamesProgress();
+gameProgress();
--- src/games/brain-progression.js
+++ src/games/brain-progression.js
@@ -12,8 +12,46 @@ greeting(desc);
 const userName = readlineSync.question('May I have your name? ');
 hello(userName);
 
+const startPosition = randomNum(10, 1);
+const step = randomNum(10, 2);
+const a = 10;
+const index = randomNum(a - 1, 0);
+let i = 0;
+
+
+
 const gameProgress = (rounds = 3) => {
   
+
+  let count = 1;
+  for (; count <= rounds; count += 1) {
+    
+    const progressionGenerated = () => {
+      let count = 1;
+      for (; count <= rounds; count += 1) {
+        
+          let progression = '';
+          for (; i < a; i += 1) {
+            if (i === index) {
+              progression = `${progression}.. `;
+            } else {
+              progression = `${progression}${startPosition + step * i} `;
+            }
+          }
+          return progression;
+          };
+      }
+    const progressionTwo = progressionGenerated();
+    console.log(`Question: ${progressionTwo}`);
+    const yourAnswer = readlineSync.question('Your answer: ');
+    if (yourAnswer === String(startPosition + step * index)) {
+      console.log('Correct!');
+    } else {
+      console.log(`'${yourAnswer}' is wrong answer ;(. Correct answer was ${String(startPosition + step * index)}.\nLet\`s try again, ${userName}!`);
+      return;
+    }
+  }
+  console.log(`Congratulation, ${userName}!`);
 };
 
 export { gameProgress as default };
