736bf1d326eae9fb55f381adc5c61d542eeaf103
Shurpo Alexander <shurpo.alexander@gmail.com>
2020-01-30 22:23:10 +0300

    refactoring game progression

--- src/games/brain-progression.js
+++ src/games/brain-progression.js
@@ -1,49 +1,30 @@
-import readlineSync from 'readline-sync';
-import {
-  greeting,
-  hello,
-  randomTwoNum,
-  successfully,
-} from '../index';
+import data from '../index';
+import randomNum from '../utils';
 
 const desc = 'Find the greatest common divisor of given numbers.';
 
-greeting(desc);
+const gameProgress = () => {
+  const startPosition = randomNum(10, 1);
+  const step = randomNum(10, 2);
+  const a = 10;
+  const index = randomNum(a - 1, 0);
 
-const userName = readlineSync.question('May I have your name? ');
-hello(userName);
-
-
-const gameProgress = (rounds = 3) => {
-  let count = 1;
-  for (; count <= rounds; count += 1) {
-    const startPosition = randomTwoNum(10, 1);
-    const step = randomTwoNum(10, 2);
-    const a = 10;
-    const index = randomTwoNum(a - 1, 0);
-    let i = 0;
-    const progressionGenerated = () => {
-      let progression = '';
-      for (; i < a; i += 1) {
-        if (i === index) {
-          progression = `${progression}.. `;
-        } else {
-          progression = `${progression}${startPosition + step * i} `;
-        }
+  const progressionGenerated = () => {
+    let progression = '';
+    for (let i = 0; i < a; i += 1) {
+      if (i === index) {
+        progression = `${progression}.. `;
+      } else {
+        progression = `${progression}${startPosition + step * i} `;
       }
-      return progression;
-    };
-    const progressionTwo = progressionGenerated();
-    console.log(`Question: ${progressionTwo}`);
-    const yourAnswer = readlineSync.question('Your answer: ');
-    if (yourAnswer === String(startPosition + step * index)) {
-      console.log('Correct!');
-    } else {
-      console.log(`'${yourAnswer}' is wrong answer ;(. Correct answer was ${String(startPosition + step * index)}.\nLet\`s try again, ${userName}!`);
-      return;
     }
-  }
-  successfully(userName);
+    return progression;
+  };
+
+  const result = progressionGenerated();
+  const answer = startPosition + step * index;
+
+  return [result, answer];
 };
 
-export { gameProgress as default };
+export default () => data(gameProgress, desc);
