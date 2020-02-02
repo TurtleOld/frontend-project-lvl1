2e4c1e5fb946049650e3b03f46695e3664264bcb
Shurpo Alexander <shurpo.alexander@gmail.com>
2020-02-02 18:14:23 +0300

    refactor inclusive max Math.random

--- src/utils.js
+++ src/utils.js
@@ -1,3 +1,3 @@
-const randomNum = (max, min) => Math.floor(Math.random() * (max - min) + min);
+const randomNum = (max, min) => Math.floor(Math.random() * (max - min + 1)) + min;
 
 export default randomNum;
