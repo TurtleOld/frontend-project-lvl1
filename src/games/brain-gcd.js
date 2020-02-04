8c5b85786f5b66f30738503f13392ff0e2590aa7
Shurpo Alexander <shurpo.alexander@gmail.com>
2020-02-04 22:13:39 +0300

    rename nod to getGcd

--- src/games/brain-gcd.js
+++ src/games/brain-gcd.js
@@ -3,10 +3,10 @@ import randomNum from '../utils';
 
 const description = 'Find the greatest common divisor of given numbers.';
 
-const nod = (a, b) => {
+const getGcd = (a, b) => {
   if (b) {
     const c = a % b;
-    return nod(b, c);
+    return getGcd(b, c);
   } return a;
 };
 
@@ -15,7 +15,7 @@ const createGameGcd = () => {
   const num2 = randomNum(100, 1);
 
   const question = `${num1} ${num2}`;
-  const answer = String(nod(num1, num2));
+  const answer = String(getGcd(num1, num2));
 
   return [question, answer];
 };
