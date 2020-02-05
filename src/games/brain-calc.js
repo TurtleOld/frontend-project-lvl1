6e0561e1e06c75cc62ea35dccce558abfb64bb3e
TurtleOld <dev@alex-turtle.name>
2020-02-05 09:10:39 +0300

    refactoring variable sign

--- src/games/brain-calc.js
+++ src/games/brain-calc.js
@@ -3,7 +3,7 @@ import randomNum from '../utils';
 
 const description = 'What is the result of the expression?';
 
-const symbol = ['+', '-', '*'];
+const symbols = ['+', '-', '*'];
 
 const getSign = (num1, num2, operator) => {
   switch (operator) {
@@ -21,8 +21,8 @@ const getSign = (num1, num2, operator) => {
 const createGameCalc = () => {
   const randomNum1 = randomNum(100, 1);
   const randomNum2 = randomNum(100, 1);
-  const number = randomNum(0, symbol.length);
-  const sign = (symbol[parseInt(number, 0)]);
+  const number = randomNum(symbols.length - 1, 0);
+  const sign = (symbols[number, 0]);
   const question = `${randomNum1} ${sign} ${randomNum2}`;
   const answer = String(getSign(randomNum1, randomNum2, sign));
 
