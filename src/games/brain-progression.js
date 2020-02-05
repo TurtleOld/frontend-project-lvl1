c988c5604a2500cab6f3ab3c14a9def18f20705d
TurtleOld <dev@alex-turtle.name>
2020-02-05 11:03:17 +0300

    refactoring func getGenerationProgression

--- src/games/brain-progression.js
+++ src/games/brain-progression.js
@@ -4,25 +4,25 @@ import randomNum from '../utils';
 const description = 'Find the greatest common divisor of given numbers.';
 const progressionLength = 10;
 
+const getGenerationProgression = (startPosition, step, index) => {
+  let progression = '';
+  for (let i = 0; i < progressionLength; i += 1) {
+    if (i === index) {
+      progression = `${progression}.. `;
+    } else {
+      progression = `${progression}${startPosition + step * i} `;
+    }
+  }
+  return progression;
+};
+
 const createGameProgression = () => {
   const startPosition = randomNum(10, 1);
   const step = randomNum(10, 2);
   const index = randomNum(progressionLength - 1, 0);
 
-  const getGenerationProgression = () => {
-    let progression = '';
-    for (let i = 0; i < progressionLength; i += 1) {
-      if (i === index) {
-        progression = `${progression}.. `;
-      } else {
-        progression = `${progression}${startPosition + step * i} `;
-      }
-    }
-    return progression;
-  };
-
   const question = String(getGenerationProgression());
-  const answer = startPosition + step * index;
+  const answer = getGenerationProgression(startPosition + step * index);
 
   return [question, answer];
 };
